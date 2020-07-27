fde085a46f2f8d3bf67244a89f3da01b4ed4baac
daubourg-clovis <daubourg.clovis@gmail.com>
2020-07-27 14:28:36 +0200

    Update - fix + remove port

--- database.php
+++ database.php
@@ -3,10 +3,10 @@
 $server = 'localhost';
 $user = 'root';
 $password = '';
-$db = 'equipment'
+$db = 'equipment';
 
 try{
-    $pdo = new PDO("mysql:host=$server;port=3308;dbname=$db", $user, $password);
+    $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $password);
 }catch(PDOExeption $e){
     echo 'connection failed' / $e->getMessage();
 }
