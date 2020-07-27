e8ca2963e230300262a437fd58648b275b48272f
daubourg-clovis <daubourg.clovis@gmail.com>
2020-07-27 15:15:13 +0200

    Update-

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
 
-$server = 'localhost';
+$host = 'localhost';
 $user = 'root';
 $password = '';
 $db = 'equipment';
 
 try{
-    $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
 }catch(PDOExeption $e){
     echo 'connection failed' / $e->getMessage();
 }
