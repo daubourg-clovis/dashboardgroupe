270d3454e0cec67d251fecfd8dcf5adc422ec9ca
Clovis Daubourg <62110612+daubourg-clovis@users.noreply.github.com>
2020-07-28 10:35:13 +0200

    Update - modification for categories table

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 $dateFr = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
 
-$sql = 'SELECT p.id, p.name, p.reference, p.category, p.purchasedate, p.warrantydate, p.price, p.purchaseticket, p.maintenance, p.usermanual, s.name, s.address FROM products AS p INNER JOIN sellers AS s ORDER BY p.purchasedate DESC';
+$sql = 'SELECT p.id, p.name, p.reference, c.type, p.purchasedate, p.warrantydate, p.price, p.purchaseticket, p.maintenance, p.usermanual, s.id, s.name, s.address FROM products AS p INNER JOIN sellers AS s INNER JOIN categories AS c ORDER BY p.purchasedate DESC';
 $sth = $pdo->prepare($sql);
 $sth->execute();
 $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -12,9 +12,9 @@ foreach($rows as $row){
     echo '<td>'.$row['p.id'].'</td>';
     echo '<td>'.$row['p.name'].'</td>';
     echo '<td>'.$row['p.reference'].'</td>';
-    echo '<td>'.$row['p.category'].'</td>';
-    echo '<td>'.$dateFr->format(strtotime($row['purchasedate'])).'</td>';
-    echo '<td>'.$dateFr->format(strtotime($row['warrantydate'])).'</td>';
+    echo '<td>'.$row['c.type'].'</td>';
+    echo '<td>'.$dateFr->format(strtotime($row['p.purchasedate'])).'</td>';
+    echo '<td>'.$dateFr->format(strtotime($row['p.warrantydate'])).'</td>';
     echo '<td>'.$row['p.price'].'</td>';
     echo '<td>'.$row['p.purchaseticket'].'</td>';
     echo '<td>'.$row['p.maintenance'].'</td>';
