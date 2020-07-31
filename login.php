955992fff42da578d2603132da0f506eb8ad913c
daubourg-clovis <daubourg.clovis@gmail.com>
2020-07-31 13:56:27 +0200

    Update - fix password verify

--- login.php
+++ login.php
@@ -4,22 +4,20 @@
 
 if(isset($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
     extract($_POST);
-    $sql = 'SELECT  username, pwd FROM membre WHERE user=:user AND pwd:pwd';
+    $sql = 'SELECT  username, pwd FROM users  WHERE user=:user AND pwd:pwd';
     $sth = $pdo->prepare($sql);
-    $sth->password_verify(':pwd');
     $sth->bindParam(':user', $user, PDO::PARAM_STR);
+    $sth->bindParam(':pwd', $pwd, PDO::PARAM_STR);
     $sth->execute();
     $result = $sth->fetch(PDO::FETCH_ASSOC);
-    if(gettype($result) !== 'boolean'){
-        if($result['pwd'] == $password){
+        if(password_verify($pwd) == $password){
             session_start();
             $_SESSION['username'] = $user;
-            header('Location: index.html.twig');
+            header('Location: index.php');
             exit;
         }else{
             echo 'Accès refusé';
         }
-    }
 }
 
 
