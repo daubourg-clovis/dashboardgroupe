0fc37814b401ffdc5a594252de10e476ac24a9e0
vaness25 <vanessa.knorr@outlook.fr>
2020-08-20 14:22:47 +0200

    ajout du fichier script2

---
+++ script2.js
@@ -0,0 +1,10 @@
+const logout=document.getElementsByClassName("logout")[0];
+
+logout.addEventListener("click",function(){
+    const deco = document.getElementById("deco");
+    deco.classList.remove("hidden");
+
+
+
+});
+
