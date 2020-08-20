10f93b309a0e060472e02cba715d61387b4401cf
vaness25 <vanessa.knorr@outlook.fr>
2020-08-20 23:56:42 +0200

    rectification

--- script2.js
+++ script2.js
@@ -1,10 +1,15 @@
 const logout=document.getElementsByClassName("logout")[0];
+const cacher=document.getElementById("cacher");
 
 logout.addEventListener("click",function(){
     const deco = document.getElementById("deco");
+
     deco.classList.remove("hidden");
 
 
-
 });
+cacher.addEventListener("click", function(){
+    const deco = document.getElementById("deco");
 
+    deco.classList.add("hidden");
+})
